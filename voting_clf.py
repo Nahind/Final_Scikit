4d50373f93ab77e2b8a235ccd183b8f47a7508db
nandane <n.djanar@gmail.com>
2017-04-10 00:01:37 -0400

    Voting classifier improved

--- voting_clf.py
+++ voting_clf.py
@@ -9,9 +9,11 @@ import personal_settings
 
 path = personal_settings.PATH
 algorithm = os.path.basename(__file__).split(".py")[0]
+datasets = os.listdir(path)
+datasets = ["MSD-SSD"]
 
 #execute for all datasets:
-for extraction_type in os.listdir(path):
+for extraction_type in datasets:
     print("Starting new classification. Extraction method : " + extraction_type)
     folder = path + extraction_type + "/"
 
@@ -19,9 +21,9 @@ for extraction_type in os.listdir(path):
 
         # Training classifiers
         clf1 = MLPClassifier(verbose=True, hidden_layer_sizes=300, early_stopping=True)
-        clf2 = KNeighborsClassifier(n_neighbors=10)
-        clf3 = ensemble.RandomForestClassifier(n_jobs=-1, verbose=20, n_estimators=15)
-        eclf = VotingClassifier(estimators=[('dt', clf1), ('knn', clf2), ('svc', clf3)], voting='soft',
+        clf2 = KNeighborsClassifier(n_neighbors=10, n_jobs=-1)
+        clf3 = ensemble.RandomForestClassifier(n_jobs=-1, verbose=20, n_estimators=50)
+        eclf = VotingClassifier(estimators=[('mlp', clf1), ('knn', clf2), ('rf', clf3)], voting='soft',
                                 weights=[2, 1, 2], n_jobs=-1)
         sdk.evaluate_classifier(eclf, folder, extraction_type, algorithm)
 
