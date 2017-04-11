4a9812da8cfec5fc21825967d94983fc59c0b4bf
root <root@vultr.guest>
2017-04-11 15:30:20 +0000

    Params modification on some classifiers

--- one_against_all.py
+++ one_against_all.py
@@ -1,4 +1,5 @@
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn import ensemble
 import personal_settings
 import SDK as sdk
@@ -6,15 +7,20 @@ import os
 
 path = personal_settings.PATH
 algorithm = os.path.basename(__file__).split(".py")[0]
+datasets = os.listdir(path)
+datasets = personal_settings.LARGE_DATASETS
 
 # execute for all datasets:
-for extraction_type in os.listdir(path):
+for extraction_type in datasets:
     print("Starting new classification. Extraction method : " + extraction_type)
     folder = path + extraction_type + "/"
+    n_trees = 100
 
     try:
-        clf = OneVsRestClassifier(ensemble.RandomForestClassifier(n_jobs=-1, verbose=20, n_estimators=60), n_jobs=-1)
-        sdk.evaluate_classifier(clf, folder, extraction_type, algorithm, suffixe="_rd_60")
+        in_clf_mlp = MLPClassifier(verbose=True, hidden_layer_sizes=300, early_stopping=True)
+        in_clf_rd = ensemble.RandomForestClassifier(n_jobs=-1, verbose=20, n_estimators=n_trees)
+        clf = OneVsRestClassifier(in_clf_mlp, n_jobs=-1)
+        sdk.evaluate_classifier(clf, folder, extraction_type, algorithm, suffixe="_mlp")
 
     except Exception as e:
         print(str(e))
--- voting_clf.py
+++ voting_clf.py
@@ -1,7 +1,9 @@
 from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import ensemble
 from sklearn.ensemble import VotingClassifier
+from sklearn.svm import SVC
 
 import SDK as sdk
 import os
@@ -10,7 +12,7 @@ import personal_settings
 path = personal_settings.PATH
 algorithm = os.path.basename(__file__).split(".py")[0]
 datasets = os.listdir(path)
-datasets = ["MSD-SSD"]
+# datasets = ["MSD-SSD"]
 
 #execute for all datasets:
 for extraction_type in datasets:
@@ -21,11 +23,13 @@ for extraction_type in datasets:
 
         # Training classifiers
         clf1 = MLPClassifier(verbose=True, hidden_layer_sizes=300, early_stopping=True)
-        clf2 = KNeighborsClassifier(n_neighbors=10, n_jobs=-1)
-        clf3 = ensemble.RandomForestClassifier(n_jobs=-1, verbose=20, n_estimators=50)
-        eclf = VotingClassifier(estimators=[('mlp', clf1), ('knn', clf2), ('rf', clf3)], voting='soft',
+        clf2 = GaussianNB()
+        # clf2 = SVC(coef0=1000.0, gamma=0.0001, verbose=True)
+        clf3 = KNeighborsClassifier(n_neighbors = 20, n_jobs = -1)
+        # clf3 = ensemble.RandomForestClassifier(n_jobs=-1, verbose=20, n_estimators=200)
+        eclf = VotingClassifier(estimators=[('mlp', clf1), ('nb', clf2), ('knn', clf3)], voting='soft',
                                 weights=[2, 1, 2], n_jobs=-1)
-        sdk.evaluate_classifier(eclf, folder, extraction_type, algorithm)
+        sdk.evaluate_classifier(eclf, folder, extraction_type, algorithm, suffixe='_mlp_nb_knn_20')
 
 
     except Exception as e:
