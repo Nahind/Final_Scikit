9f8cf2a63aeb71db6834d46911fef50dd968a1d9
root <root@vultr.guest>
2017-04-12 01:25:11 +0000

    local small changes

--- adaboost_nb.py
+++ adaboost_nb.py
@@ -8,12 +8,12 @@ import personal_settings
 
 path = personal_settings.PATH
 algorithm = "adaboost_nn"
-suffixe = "_neural_net"
 
 
-datasets = personal_settings.SMALL_DATASETS
+datasets = personal_settings.ALL_DATASETS
 
 for extraction_type in datasets:
+
     print("Starting new classification. Extraction method : " + extraction_type)
     folder = path + extraction_type + "/"
 
@@ -22,24 +22,36 @@ for extraction_type in datasets:
         training, validation = sdk.load_dataset_from_folder(folder, extraction_type)
         # Normalize training & validation sets
         print("Start normalizing data")
-        scaler.fit(training.data)
-        training.data = scaler.transform(training.data)
+        # scaler.fit(training.data)
+        # training.data = scaler.transform(training.data)
         print("Training data has been normalized")
-        validation.data = scaler.transform(validation.data)
+        # validation.data = scaler.transform(validation.data)
         print("Validation data has been normalized")
         # Build MLP model
-        print("Start training MLP Classifier")
-        clf = MLPClassifier(verbose=True, hidden_layer_sizes=300, early_stopping=True, validation_fraction=0.2)
+        print("Start training MLP Classifier with adaboost")
+        # clf = MLPClassifier(verbose=True, hidden_layer_sizes=300, early_stopping=True)
         # Create and fit an AdaBoosted MLP
-        bdt = AdaBoostClassifier(clf, algorithm="SAMME", n_estimators=500)
-        model = clf.fit(training.data, training.target)
+#        bdt = AdaBoostClassifier(clf, algorithm="SAMME", n_estimators=500)
+ #       model = bdt.fit(training.data, training.target)
         print("Training has ended")
         # Save MLP model
-        sdk.save_model(model, extraction_type, algorithm)
+        # sdk.save_model(model, extraction_type, algorithm)
         # Evaluation model
+        print("Start predicting validation set")
+  #      y_pred = model.predict(validation.data)
+        # Save Evaluation report
+   #     suffixe = "_neural_net"
+    #    sdk.save_classification_report(validation, extraction_type, y_pred, algorithm, suffixe=suffixe)
+
+        print("Start training MLP Classifier with decision trees")
+        # Create and fit an AdaBoosted MLP
+        bdt = AdaBoostClassifier(n_estimators=500)
+        model = bdt.fit(training.data, training.target)
+        print("Training has ended")
         print("Start predicting validation set")
         y_pred = model.predict(validation.data)
         # Save Evaluation report
+        suffixe = "_decision_trees"
         sdk.save_classification_report(validation, extraction_type, y_pred, algorithm, suffixe=suffixe)
     
     except Exception as e:
